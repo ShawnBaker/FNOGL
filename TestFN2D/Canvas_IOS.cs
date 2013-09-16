36b55704e1b73922f8d902bf7e94ac51398ad8e7
Shawn Baker <shawn@frozen.ca>
2013-09-16 02:08:49 -0500

    Gestures fail if there's a modal control.

--- TestFN2D/Canvas_IOS.cs
+++ TestFN2D/Canvas_IOS.cs
@@ -1,3 +1,26 @@
+/*******************************************************************************
+*
+* Copyright (C) 2013 Frozen North Computing
+*
+* Permission is hereby granted, free of charge, to any person obtaining a copy
+* of this software and associated documentation files (the "Software"), to deal
+* in the Software without restriction, including without limitation the rights
+* to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+* copies of the Software, and to permit persons to whom the Software is
+* furnished to do so, subject to the following conditions:
+*
+* The above copyright notice and this permission notice shall be included in
+* all copies or substantial portions of the Software.
+*
+* THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+* IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+* FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+* AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+* LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+* OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+* THE SOFTWARE.
+*
+*******************************************************************************/
 using System;
 using System.Drawing;
 using MonoTouch.UIKit;
@@ -27,6 +50,11 @@ namespace FrozenNorth.TestFN2D
 		
 		private void HandlePinch()
 		{
+			if (ModalControl != null)
+			{
+				pinchGestureRecognizer.State = UIGestureRecognizerState.Failed;
+				return;
+			}
 			if (TouchControl != null)
 			{
 				TouchControl.TouchCancel(FN2DTouchEventArgs.Empty);
@@ -54,6 +82,11 @@ namespace FrozenNorth.TestFN2D
 
 		public void HandlePan()
 		{
+			if (ModalControl != null)
+			{
+				panGestureRecognizer.State = UIGestureRecognizerState.Failed;
+				return;
+			}
 			if (TouchControl != null)
 			{
 				TouchControl.TouchCancel(FN2DTouchEventArgs.Empty);
